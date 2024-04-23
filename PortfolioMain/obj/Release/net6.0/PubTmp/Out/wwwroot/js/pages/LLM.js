c31b3b41f6a6c74005ccae7b4ae373b5aef309ac
Parker Bidigare <work.simpleintelligence@gmail.com>
2024-04-22 21:14:58 -0400

    removed the gitignore for now, will have to remake it since it was leaving out files I needed. Never copy and paste a generic vs based gitignore

---
+++ PortfolioMain/obj/Release/net6.0/PubTmp/Out/wwwroot/js/pages/LLM.js
@@ -0,0 +1,59 @@
+﻿var net = new brain.recurrent.LSTM({
+    hiddenLayers: [10, 20, 10],  
+    learningRate: 0.01,      
+    decayRate: 0.999,        
+    errorThresh: 0.005       
+});
+var ajax = new AjaxCaller();
+
+async function main() {
+    $.ajax({
+        url: '/brainjs-model', 
+        type: 'GET',           
+        dataType: 'json',      
+        contentType: 'application/json',
+        success: function (model) {
+            console.log(model);
+            net.fromJSON(model);
+
+            document.getElementById('result').innerHTML = brain.utilities.toSVG(
+                net,
+                {
+                    height: '1000',
+                    width: '1000',
+                    radius: '5',
+                    line: {
+                        width: '0.2',
+                        color: 'rgba(100,149,237,0.5)'
+                    }
+                    /*inputs:{
+                       */
+                }
+            );
+
+            $('body').find('#brainjsBtn').on('click', function (e) {
+                let prompt = $('body').find('#brainjsTxt').val();
+                let response = net.run(prompt);
+                $('body').find('#brainjsChatLog').append(`${response}<br/>`);
+            });
+
+            $('body').find('#chatGPTBtn').on('click', function (e) {
+                let prompt = $('body').find('#chatGPTtxt').val();
+                ajax.performCall('POST', '/langchain', { phrase: prompt }).then((data) => {
+                    console.log('langchain data: ', data);
+                })
+                    .catch((err) => {
+                        console.error(err);
+                    })
+            });
+        },
+        error: function (a, b, c) {
+            console.log(a);
+            console.log(b);
+        }
+    });
+
+}
+
+
+main();
---
+++ PortfolioMain/obj/Release/net6.0/PubTmp/Out/wwwroot/js/pages/WebXR.js
@@ -0,0 +1,40 @@
+﻿$(document).ready(function () {
+    console.log('base url: ', baseUrl);
+    var images = [baseUrl + "assets/QR/install_app.png", baseUrl  + "assets/QR/website_webxr_url.png"];
+    var texts = ["Please scan this QR code with your smart phone to get WebXR Viewer app (currently only iOS)", "Now scan this QR code to go to this webpage on your phone, with the WebXR Viewer browser."];
+    var currentIndex = 0;
+    var totalImages = images.length;
+
+    $('.next').click(function () {
+        // Move current content out of view
+        $('.modal-image').addClass('hidden-left');
+        $('.modal-text').addClass('hidden-left');
+
+        setTimeout(function () {
+            // Update to next content set
+            currentIndex = (currentIndex + 1) % totalImages;
+            $('.modal-image').attr('src', images[currentIndex]);
+            $('.modal-text').text(texts[currentIndex]);
+
+            // Move new content into view from the right
+            $('.modal-image').removeClass('hidden-left').addClass('hidden-right');
+            $('.modal-text').removeClass('hidden-left').addClass('hidden-right');
+
+            requestAnimationFrame(function () {
+                $('.modal-image').removeClass('hidden-right');
+                $('.modal-text').removeClass('hidden-right');
+            });
+
+            // Check if we need to change the button to 'Finish'
+            if (currentIndex === totalImages - 1) {
+                $('.next').text('Finish').removeClass('next').addClass('finish');
+            }
+        }, 500); // Match the CSS transition time
+    });
+
+    // New handler for the finish button
+    $(document).on('click', '.finish', function () {
+        $('#exampleModal').modal('hide');
+    });
+});
+
